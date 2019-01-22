9555b9dece13618c2f64751562b948847667aa00
Goren G <yong.gu@qlink.mobi>
2019-01-22 17:37:10 +0800

    fix: CI failed

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ require('es6-promise').polyfill();
 import * as con from "./common";
 import * as u from "./utils";
 import * as c from "./client";
-import * as w from "./Wallet";
+import * as w from "./wallet";
 
 export const common = con;
 export const utils = u;
